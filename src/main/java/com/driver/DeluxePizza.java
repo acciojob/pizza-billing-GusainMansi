80b7b375fd28da908d38df393b0b5217bfd12568
GusainMansi <mansi.eeebt18@nituk.ac.in>
2022-11-21 16:28:32 +0530

    Adding files

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,13 +5,15 @@ private int price;
 private String bill;
 boolean isVeg;
 
-    public boolean checkToppings=true;
-
-    public boolean checkExtraCheese=true;
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
           this.price= super.getPrice();
+        if(isVeg){
+            this.price+=80+70;
+        }
+        else this.price+=80+120;
+
 
     }
 
@@ -35,10 +37,6 @@ boolean isVeg;
     @Override
     public String getBill() {
 
-        if(isVeg){
-            this.price+=80+70;
-        }
-        else this.price+=80+120;
         this.bill=Integer.toString(price);
         return this.bill;
     }
