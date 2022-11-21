a53b5958af7bcf1d1fe50eed59d5e29f0c5da6ed
GusainMansi <mansi.eeebt18@nituk.ac.in>
2022-11-21 13:37:34 +0530

    Adding pizza files

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -1,9 +1,19 @@
 package com.driver;
 
 public class DeluxePizza extends Pizza {
-
+private int price;
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+        if(isVeg){
+           price=(300+80+70);
+
+        }
+        else price=(300+80+120);
+
+
+
     }
+
+
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,15 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
-
+public boolean check=false;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            price=300;
+        }
+        else price=400;
+
     }
 
     public int getPrice(){
@@ -17,14 +22,19 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        price+=80;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        price+=70;
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!check)
+        price+=20;
+        check=true;
     }
 
     public String getBill(){
