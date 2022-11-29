0a8c6bdb3c6a88797e1261d8b41c3ba2a61df52e
GusainMansi <mansi.eeebt18@nituk.ac.in>
2022-11-29 22:29:20 +0530

    a

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,18 +5,16 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-Scanner sc= new Scanner(System.in);
-boolean ans= Boolean.parseBoolean(sc.next());
 
 
-    DeluxePizza dp = new DeluxePizza(ans);
+    DeluxePizza dp = new DeluxePizza(true);
     System.out.println(dp.getPrice());
     dp.addTakeaway();
     dp.addExtraCheese();
     dp.addTakeaway();
     System.out.println(dp.getBill());
 
-    Pizza p = new Pizza(ans);
+    Pizza p = new Pizza(true);
     p.addExtraCheese();
     p.addExtraToppings();
    p.addTakeaway();
